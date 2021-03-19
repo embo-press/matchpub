ae0ec57bb4abd0b3dfda9bb042bdbe641142d962
Thomas Lemberger <thomas.lemberger@embo.org>
2021-03-19 07:27:24 +0100

    bogus run

--- bogus_run.py
+++ bogus_run.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 if __name__ == "__main__":
     hi = "hello code ocean! I cannot build with docker-compose build or run with docker-compose run --rm matchpub python -m src.scan /data/test-data.xls /results/test-data"
-    print(text)
-    Path("./hi.txt").write_text(hi). # how can I put this in results/?
+    print(hi)
+    Path("/results/hi.txt").write_text(hi)  # how can I put this in results/?
