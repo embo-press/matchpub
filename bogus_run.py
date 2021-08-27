e210d849491db8d8b22de4fd6c5f82bbd2c0c9fc
Thomas Lemberger <thomas.lemberger@embo.org>
2021-08-27 12:06:55 +0200

    remove Code Ocean bogus run

--- bogus_run.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-if __name__ == "__main__":
-    hi = "hello code ocean! I cannot build with docker-compose build or run with docker-compose run --rm matchpub python -m src.scan /data/test-data.xls /results/test-data"
-    print(hi)
-    Path("/results/hi.txt").write_text(hi)  # how can I put this in results/?
